6c869a9f9dec57748a954ae7559dc0cd2d2c2b92
prempaolo <prempaolo@gmail.com>
2020-06-09 13:46:11 +0200

    fixes on fonts

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Hack Nerd Font:pixelsize=15:antialias=true:autohint=true";
+static char *font = "MesloLGS Nerd Font:pixelsize=15:antialias=true:autohint=true";
 static char *font2[] = { "JoyPixels:pixelsize=10:antialias=true:autohint=true" };
 static int borderpx = 2;
 
